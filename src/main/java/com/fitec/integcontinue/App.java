2619e9cf9288c26541dbdafa3866aa9e58630d3d
Jemal Ahmed <ahmed.jmal@dksoft.tn>
2022-04-22 22:15:59 +0200

    inital commit

---
+++ src/main/java/com/fitec/integcontinue/App.java
@@ -0,0 +1,13 @@
+package com.fitec.integcontinue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
